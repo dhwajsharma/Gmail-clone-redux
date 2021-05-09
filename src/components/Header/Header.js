7920f83805427335f667d8a6ba9b7be8d582ae3b
DhwajSharma <dhwajsharma10@gmail.com>
2021-05-09 23:30:32 +0530

    Done with header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,35 @@
+import { Avatar, Icon, IconButton } from '@material-ui/core'
+import { Apps, ArrowDropDown, Menu, Notifications, Search } from '@material-ui/icons'
+import React from 'react'
+import "./Header.css"
+
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="header_left">
+                <IconButton>
+                    <Menu />
+                </IconButton>
+                <img src="https://1000logos.net/wp-content/uploads/2018/05/Gmail-logo.png" alt="" />
+            </div>
+
+            <div className="header_middle">
+                <Search />
+                <input type="text" placeholder="Search Mail" />
+                <ArrowDropDown className="header_inputCaret" />
+            </div>
+
+            <div className="header_right">
+                <Icon>
+                    <Apps />
+                </Icon>
+                <Icon>
+                    <Notifications />
+                </Icon>
+                <Avatar />
+            </div>
+        </div>
+    )
+}
+
+export default Header
