8fb6df346cb05e314fa80d31a1a9c478892e51e4
Dhwaj Sharma <dhwajsharma10@gmail.com>
2021-05-22 20:31:09 +0530

    Built Login

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import Header from './components/Header/Header';
 import Sidebar from './components/Sidebar/Sidebar';
@@ -11,29 +11,51 @@ import {
   Link
 } from "react-router-dom";
 import SendMail from './components/SendMail/SendMail';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { selectSendMessageIsOpen } from './features/mailSlice';
+import { login, selectUser } from './features/userSlice';
+import Login from './components/Login/Login';
+import { auth } from './firebase';
 
 function App() {
-  const sendMessageIsOpen = useSelector(selectSendMessageIsOpen)
+  const sendMessageIsOpen = useSelector(selectSendMessageIsOpen);
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    auth.onAuthStateChanged(user => {
+      if (user) {
+        dispatch(login({
+          displayName: user.displayName,
+          email: user.email,
+          photoUrl: user.photoUrl,
+        }))
+      }
+    })
+  }, [dispatch])
 
   return (
     <Router>
-      <div className="App">
-        <Header />
-        <div className="app_body">
-          <Sidebar />
-          <Switch>
-            <Route path="/mail">
-              <Mail />
-            </Route>
-            <Route path="/" exact>
-              <EmailList />
-            </Route>
-          </Switch>
+
+      {!user ? (
+        <Login />
+      ) : (
+        <div className="App">
+          <Header />
+          <div className="app_body">
+            <Sidebar />
+            <Switch>
+              <Route path="/mail">
+                <Mail />
+              </Route>
+              <Route path="/" exact>
+                <EmailList />
+              </Route>
+            </Switch>
+          </div>
+          {sendMessageIsOpen && <SendMail />}
         </div>
-        {sendMessageIsOpen && <SendMail />}
-      </div>
+      )}
     </Router>
   );
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,22 @@
 import { Avatar, IconButton } from '@material-ui/core'
 import { Apps, ArrowDropDown, Menu, Notifications, Search } from '@material-ui/icons'
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { logout, selectUser } from '../../features/userSlice'
+import { auth } from '../../firebase'
 import "./Header.css"
 
 const Header = () => {
+    const user = useSelector(selectUser);
+    const dispatch = useDispatch();
+
+    const signOut = () => {
+        auth.signOut().then(() => {
+            dispatch(logout())
+        })
+    }
+
+
     return (
         <div className="header">
             <div className="header_left">
@@ -26,7 +39,7 @@ const Header = () => {
                 <IconButton >
                     <Notifications />
                 </IconButton>
-                <Avatar />
+                <Avatar className="header_avatar" onClick={signOut} src={user?.photoUrl} />
             </div>
         </div>
     )
